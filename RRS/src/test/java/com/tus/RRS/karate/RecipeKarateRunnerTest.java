2813a9482ac96e163a75728c6caaad4a2c198737
Yu Hang <A00335601@student.tus.ie>
2026-03-16 21:59:34 +0000

    To make jenkins run without running application

--- RRS/src/test/java/com/tus/RRS/karate/RecipeKarateRunnerTest.java
+++ RRS/src/test/java/com/tus/RRS/karate/RecipeKarateRunnerTest.java
@@ -1,7 +1,10 @@
 package com.tus.RRS.karate;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 import com.intuit.karate.junit5.Karate;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class RecipeKarateRunnerTest {
 
     @Karate.Test
